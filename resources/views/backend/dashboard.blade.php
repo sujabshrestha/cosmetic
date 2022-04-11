f3e018c7151f5923cf0ec3369bed2537b8509b70
sujab shrestha <ssujab9@gmail.com>
2022-04-11 09:13:58 +0545

    admin panel integrated

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.backend.master')
+
+
+@section('content')
+    <div class="jumbotron">
+        <h3>hello</h3>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'backend'
+],function(){
+    Route::get('dashboard','AdminController@index')->name('dashboard');
+});
